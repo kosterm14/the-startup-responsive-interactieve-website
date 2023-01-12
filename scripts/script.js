c3d7e7021dc6f3f59e5f7de37dc443297a4c5c57
Marco <marco.kosters@hva.nl>
2023-01-12 19:53:35 +0100

    eerste versie
    eerste ontwerp van de avocado media site (mobile first)

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// window.onscroll = function() {stick()};
+
+// var header = document.getElementById("sticky-header");
+
+// var sticky = header.offsetTop;
+
+// function stick() {
+//   if (window.pageYOffset > sticky) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// }
