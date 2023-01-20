344a28c30047125405b666393beaa32c30648caa
Marco <marco.kosters@hva.nl>
2023-01-20 20:33:07 +0100

    versie 2
    site meer op voorbeeld laten lijken en animaties toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,25 @@
-// window.onscroll = function() {stick()};
+window.onscroll = function() {stick()};
 
-// var header = document.getElementById("sticky-header");
+var header = document.getElementById("sticky-header");
 
-// var sticky = header.offsetTop;
+var sticky = header.offsetTop;
 
-// function stick() {
-//   if (window.pageYOffset > sticky) {
-//     header.classList.add("sticky");
-//   } else {
-//     header.classList.remove("sticky");
-//   }
-// }
+function stick() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+let the = document.querySelector('button')
+the.addEventListener('click', juiceHandler)
+the.addEventListener('animationend', juiceHandler)
+
+function juiceHandler() {
+  the.classList.toggle('juice')
+}
+
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 500 );
+}
